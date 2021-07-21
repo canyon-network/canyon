fe0fa2906d04fe63961dfedba8c0a28160b4e83e
liuchengxu <xuliuchengxlc@gmail.com>
2021-07-21 09:17:58 +0000

    Deploying to gh-pages from  @ fdfab58a4f695a758e521bf79a7267bbdff1ecb9 🚀

--- implementors/sp_api/trait.RuntimeApiInfo.js
+++ implementors/sp_api/trait.RuntimeApiInfo.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["cp_permastore"] = [{"text":"impl&lt;Block:&nbsp;BlockT, BlockNumber, ExtrinsicIndex, Hash&gt; RuntimeApiInfo for dyn <a class=\"trait\" href=\"cp_permastore/trait.PermastoreApi.html\" title=\"trait cp_permastore::PermastoreApi\">PermastoreApi</a>&lt;Block, BlockNumber, ExtrinsicIndex, Hash&gt;","synthetic":false,"types":["cp_permastore::PermastoreApi"]}];
+implementors["cp_permastore"] = [{"text":"impl&lt;Block:&nbsp;BlockT, BlockNumber, ExtrinsicIndex, Hash&gt; RuntimeApiInfo for dyn <a class=\"trait\" href=\"cp_permastore/trait.PermastoreApi.html\" title=\"trait cp_permastore::PermastoreApi\">PermastoreApi</a>&lt;Block, BlockNumber, ExtrinsicIndex, Hash&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
