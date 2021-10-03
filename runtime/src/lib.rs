79d8220cfd70f1a12542dca24f924f063d572ccd
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-10-03 14:26:30 +0800

    Simplify @ _

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -575,7 +575,7 @@ impl frame_support::pallet_prelude::Get<Option<(usize, sp_npos_elections::Extend
         use sp_runtime::traits::TrailingZeroInput;
         let iters = match MINER_MAX_ITERATIONS {
             0 => 0,
-            max @ _ => {
+            max => {
                 let seed = sp_io::offchain::random_seed();
                 let random = <u32>::decode(&mut TrailingZeroInput::new(&seed))
                     .expect("input is padded with zeroes; qed")
